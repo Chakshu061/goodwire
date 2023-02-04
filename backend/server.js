48eca29a520bf0c1c66f363d6a0eb7d422768f07
Chakshu Shaktawat <93027256+Chakshu061@users.noreply.github.com>
2023-02-04 17:47:17 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+//config
+dotenv.config({path:"backend/config/config.env"})
+
+//connecting to database
+connectDatabase();
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+})
